bb564133e1a32ac3365173b63238b54cda4dce86
TanmayLokhande <84788186+TanmayLokhande@users.noreply.github.com>
2022-12-15 20:01:48 +0530

    Delete Blog-backend directory

--- Blog-backend/src/main/java/com/blog/Repositories/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.blog.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.blog.Entities.Category;
-
-public interface CategoryRepo extends JpaRepository<Category, Integer>{
-
-}
