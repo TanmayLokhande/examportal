0affa326be0800d6f959a07c8e5e8ad02e1cc4e9
TanmayLokhande <84788186+TanmayLokhande@users.noreply.github.com>
2022-12-15 20:02:29 +0530

    Delete Ecommerce-backend directory

--- Ecommerce-backend/src/main/java/com/ecommerce/Utils/passwordHelper.java
+++
@@ -1,5 +0,0 @@
-package com.ecommerce.Utils;
-
-public class passwordHelper {
-
-}
--- Ecommerce-backend/src/main/java/com/ecommerce/payloads/JwtAuthRespponse.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.payloads;
-
-import lombok.Data;
-
-@Data
-public class JwtAuthRespponse {
-
-	private String token;
-}
